53873bff100c36b6bd27268776438e0c68deeb68
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-10-27 14:45:19 +0100

    add AddressEntity

--- src/main/java/de/tekup/rest/data/models/PersonEntity.java
+++ src/main/java/de/tekup/rest/data/models/PersonEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -25,6 +26,7 @@ public class PersonEntity {
 	
 	private LocalDate dateOfBirth;
 	
-	private String address;
+	@OneToOne
+	private AddressEntity address;
 
 }
